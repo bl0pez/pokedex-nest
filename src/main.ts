3e133caff491c2e59e1c220d73b8608524071fba
b0pez <blopez4434@gmail.com>
2022-10-11 20:15:33 -0300

    metodo post

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -7,6 +8,14 @@ async function bootstrap() {
   //  setGlobalPrefix() is used to set a global prefix for all routes
   //  setGlobalPrefix() se utiliza para establecer un prefijo global para todas las rutas
   app.setGlobalPrefix('api/v2');
+  app.useGlobalPipes(
+    // Instalar npm install class-validator class-transformer
+    // para que funcione el pipe de validacion
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
 
   await app.listen(3000);
 }
